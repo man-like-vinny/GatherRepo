e026ff34972be28104ba2da9a696972b5ea5af70
man-like-vinny <vinayakunnithan@yahoo.com>
2023-10-24 18:20:36 +0100

    Removing MongoDB to fix bugs

--- server.js
+++ server.js
@@ -71,32 +71,32 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
-const mongoURI = 'mongodb+srv://vinayakunnithan:Vinayak1@gather.fgw0v5i.mongodb.net/checkoutusers'; // Update with your MongoDB URI
+//const mongoURI = 'mongodb+srv://vinayakunnithan:Vinayak1@gather.fgw0v5i.mongodb.net/checkoutusers'; // Update with your MongoDB URI
 
 // Connect to MongoDB
-mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+// mongoose.connect(mongoURI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
 
 // Define a schema for your data
-const checkoutSchema = new mongoose.Schema({
-    name: String,
-    phone: String,
-    address: String,
-    country: String,
-    city: String,
-    totalquantity: Number,
-    totalprice: Number
-});
+// const checkoutSchema = new mongoose.Schema({
+//     name: String,
+//     phone: String,
+//     address: String,
+//     country: String,
+//     city: String,
+//     totalquantity: Number,
+//     totalprice: Number
+// });
 
-const Checkout = mongoose.model('Checkout', checkoutSchema);
+//const Checkout = mongoose.model('Checkout', checkoutSchema);
 const stripe = require("stripe")('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');
 
 app.use(express.json());
@@ -107,40 +107,40 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/submit', (req, res) => {
-    const { name, phone, address, country, city, totalquantity, totalprice } = req.body;
+// app.post('/submit', (req, res) => {
+//     const { name, phone, address, country, city, totalquantity, totalprice } = req.body;
 
-    const newCheckout = new Checkout({
-        name,
-        phone,
-        address,
-        country,
-        city,
-        totalquantity,
-        totalprice,
-    });
+//     const newCheckout = new Checkout({
+//         name,
+//         phone,
+//         address,
+//         country,
+//         city,
+//         totalquantity,
+//         totalprice,
+//     });
 
-    newCheckout.save()
-        .then(() => {
-            console.log('Received data:', req.body);
-            res.status(200).send('Data saved to MongoDB');
-        })
-        .catch((err) => {
-            console.error('Error saving to MongoDB:', err);
-            res.status(500).send('Error saving data');
-        });
-});
+//     newCheckout.save()
+//         .then(() => {
+//             console.log('Received data:', req.body);
+//             res.status(200).send('Data saved to MongoDB');
+//         })
+//         .catch((err) => {
+//             console.error('Error saving to MongoDB:', err);
+//             res.status(500).send('Error saving data');
+//         });
+// });
 
-app.get('/getCheckouts', (req, res) => {
-    Checkout.find({}, 'name phone address')
-        .then((data) => {
-            res.json(data);
-        })
-        .catch((err) => {
-            console.error('Error retrieving data:', err);
-            res.status(500).send('Error retrieving data');
-        });
-});
+// app.get('/getCheckouts', (req, res) => {
+//     Checkout.find({}, 'name phone address')
+//         .then((data) => {
+//             res.json(data);
+//         })
+//         .catch((err) => {
+//             console.error('Error retrieving data:', err);
+//             res.status(500).send('Error retrieving data');
+//         });
+// });
 
 const calculateOrderAmount = (items) => {
   // Replace this constant with a calculation of the order's amount
